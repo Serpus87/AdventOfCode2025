0697cc159c04037c8c0ae0fa6b6e3d0379028e6e
Serpus87 <cesarvroom@gmail.com>
2025-12-01 13:34:55 +0100

    added test project

---
+++ AdventOfCode2025.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2025.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
