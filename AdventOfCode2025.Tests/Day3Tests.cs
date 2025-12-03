8f77f07389ea3db99d98707b6d6b8256f576d758
Serpus87 <cesarvroom@gmail.com>
2025-12-03 13:29:38 +0100

    tests green

--- AdventOfCode2025.Tests/Day3Tests.cs
+++ AdventOfCode2025.Tests/Day3Tests.cs
@@ -24,4 +24,37 @@ public class Day3Tests
         // Assert
         Assert.AreEqual(expectedResult, result);
     }
+
+    [DataRow("987654321111111", 987654321111ul)]
+    [DataRow("811111111111119", 811111111119ul)]
+    [DataRow("234234234234278", 434234234278ul)]
+    [DataRow("818181911112111", 888911112111ul)]
+    [TestMethod]
+    public void Part3Solve_BankInput_ReturnsExpectedResult(string line, ulong expectedResult)
+    {
+        // Arrange
+        var joltages = line.ToCharArray().Select(x => uint.Parse(x.ToString())).ToList(); ;
+        var bank = new Bank(joltages);
+
+        // Act
+        var result = Part2.Solve(new List<Bank> { bank });
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [TestMethod]
+    public void Part3Solve_Example_ReturnsExpectedResult()
+    {
+        // Arrange
+        var expectedResult = 3121910778619u;
+        var fileName = "Example.txt";
+        var banks = InputReader.GetBanks(fileName);
+
+        // Act
+        var result = Part2.Solve(banks);
+
+        // Assert
+        Assert.AreEqual(expectedResult, result);
+    }
 }
