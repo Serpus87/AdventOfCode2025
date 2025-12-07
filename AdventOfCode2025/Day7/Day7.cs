020be2146ff0c50a4e806c570c0a64125c43f0ab
Serpus87 <cesarvroom@gmail.com>
2025-12-07 13:54:50 +0100

    setup day7

---
+++ AdventOfCode2025/Day7/Day7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025.Day7;
+
+public static class Day7
+{
+    public static void Solve()
+    {
+        // read file
+        var fileName = "PuzzleInput.txt";
+        //var input = InputReader.GetProblems(fileName);
+
+        var solutionPart1 = Part1.Solve();
+
+        // firstTry: 
+        Console.WriteLine($"Day6 Part1 Solution: {solutionPart1}");
+
+        // firstTry:
+        var solutionPart2 = Part2.Solve();
+
+        // firstTry: 
+        Console.WriteLine($"Day6 Part2 Solution: {solutionPart2}");
+    }
+}
